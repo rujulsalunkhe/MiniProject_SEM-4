25c75d144b6dfa0203afa31d773017d4a1b8d28d
Avantika Sharma <avantikasharma405@gmail.com>
2024-03-07 21:48:38 +0530

    db first listing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/homebeing";
 main().then( () => {
@@ -16,6 +17,18 @@ app.get("/", (req, res) => {
     res.send("hi, I am root");
 });
 
+app.get("/testListing", async (req,res) => {
+ let sampleListing = new Listing({
+    title: "wooden kitchen",
+    description: "Thane",
+    price : 1200,
+    location: "Thane",
+   });
+
+   await sampleListing.save();
+   console.log("Sample was saved");
+   res.send("Successful testing");
+});
 app.listen(3000, () =>{
     console.log("Server is listening to port 3000");
 });
