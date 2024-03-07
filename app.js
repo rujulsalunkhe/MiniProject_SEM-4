2fc2e4d77979b8faf49830ab9c0392452c0e1d36
Avantika Sharma <avantikasharma405@gmail.com>
2024-03-07 19:03:51 +0530

    Basic set up

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const MONGO_URL="mongodb://127.0.0.1:27017/homebeing";
+main().then( () => {
+    console.log("Connected to DB");
+})
+.catch((err) => {
+    console.log(err);
+});
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+app.get("/", (req, res) => {
+    res.send("hi, I am root");
+});
+
+app.listen(3000, () =>{
+    console.log("Server is listening to port 3000");
+});
