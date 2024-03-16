e65c183a1064a8d30d275da787f08c1f715d4c07
Avantika Sharma <avantikasharma405@gmail.com>
2024-03-16 19:10:29 +0530

    logout button

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    console.log(req.user);
     if(!req.isAuthenticated()) {
         return req.redirect("login/login.ejs");
      }
      next();
-}
+};
